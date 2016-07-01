1df768976d52a0e0d6eec89665d0a9b6e3e55ac8
qouyang <qouyang@ctripc.com>
2016-07-01 15:02:12 +0800

    update script

---
+++ install_vim.sh
@@ -0,0 +1,7 @@
+cp .vimrc ~/.vimrc
+cd  ~/.vim/bundle
+git clone https://github.com/scrooloose/nerdtree.git
+git clone https://github.com/VundleVim/Vundle.vim.git
+git clone https://github.com/vim-scripts/taglist.vim.git
+
+
